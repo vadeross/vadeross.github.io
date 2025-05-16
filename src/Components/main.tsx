1a09f7876f9c442462058f6e4ac1650436ae0709
vadeross <dabstanislaw96@gmail.com>
2025-05-16 11:03:26 +0200

    ci: second fix

--- src/Components/main.tsx
+++ src/Components/main.tsx
@@ -1,6 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "/src/Styles/Main.scss";
-console.log(App);
-createRoot(document.getElementById("root")).render(<App />);
+
+createRoot(document.getElementById("root")!).render(<App />);
